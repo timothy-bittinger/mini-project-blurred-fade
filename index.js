0105fd2a52771dfb075b1b75fa7dd6f329698374
timothy-bittinger <timothy.r.bittinger@gmail.com>
2022-02-10 17:33:27 -0500

    Add Notes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+/*Bringing in query selectors and assigning them to to variables*/
 const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
 
+/*A function that counts upwards from 0 to 100.*/
 let load= 0;
-
 let int = setInterval(blurring, 30)
 
 function blurring() {
@@ -11,12 +12,13 @@ function blurring() {
     if(load > 99) {
         clearInterval(int);
     }
-
+/*Changes the loading percentage from 100 to 0 and the pixel blur of the background from 30px to 0.*/
     loadText.innerHTML = `${load}%`;
     loadText.style.opacity = scale(load, 0, 100, 1, 0);
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px`;
 }
 
+/*A function that scales inputs into outputs*/
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num -in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
